6552c1bb4338f920954b781c557785a00984ef75
Kevin Drake <kevindrake80@gmail.com>
2021-12-07 15:30:14 -0700

    Changed Output
    Deleted one line of Output on the console from Ch12/WriteData.java

--- Chapter 12/WriteData.java
+++ Chapter 12/WriteData.java
@@ -9,10 +9,9 @@ import java.util.*;
 public class WriteData {
 	public static void main(String[] args) throws java.io.IOException {
 		java.io.File file = new java.io.File("Exercise12_15.txt");
-		if (file.exists()) {
-			System.out.println("File already Exists");
+		if (file.exists()) 
 			System.exit(1);
-		}
+		
 		java.io.PrintWriter output = new java.io.PrintWriter(file);
 		for (int i = 0; i < 100; i++) {
 			output.print((int)(Math.random() * 100) + " ");
